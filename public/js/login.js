122e32a20a83b73d64573f0d431525f0aec96933
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2022-11-07 23:05:59 +1100

    finished modifying login and logout js

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#sign-in-form-username').value.trim();
+  const password = document.querySelector('#sign-in-form-password').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
@@ -17,37 +17,12 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({name, email, password}),
-      headers: {'Content-Type': 'application/json'},
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
+      alert('Incorrect username or password!');
     }
   }
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('.form-sign-in')
   .addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
